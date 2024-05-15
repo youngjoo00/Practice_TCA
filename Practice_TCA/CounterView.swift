9761ec2bc89ca37c9eff20442ba0061a7111add5
youngjoo00 <yungjoo0624@gmail.com>
2024-05-16 00:36:06 +0900

    first commit

---
+++ Practice_TCA/CounterView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Practice_TCA
+//
+//  Created by youngjoo on 5/16/24.
+//
+
+import SwiftUI
+
+struct CounterView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CounterView()
+}
---
+++ Practice_TCA/Practice_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_TCAApp.swift
+//  Practice_TCA
+//
+//  Created by youngjoo on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Practice_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CounterView()
+        }
+    }
+}
